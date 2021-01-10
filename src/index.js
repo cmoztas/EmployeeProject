b760d0023b9f7f1f9ba676abb355ea567b01709b
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-01-10 23:04:55 +0300

    Getting ready :)
    JSON-server installed, request class has been written and tested.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Test");
+import {Request} from "./requests";
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeeList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const req = new Request("http://localhost:3000/employees")
